676f8c55f2856e8aa406d71c3f3328a599021002
collins <tckiprotich@gmail.com>
2022-09-14 04:16:57 -0700

    please work

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+<include "0-putchar.h"
+ /*
+  *main - prnts 0-putchar.c the end with new line
+  *return -returns zero always
+  */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return 0;
+}
