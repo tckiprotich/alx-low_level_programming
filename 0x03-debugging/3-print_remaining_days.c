3d588f9423318f79165d22291e728d342e7036cb
collins <tckiprotich@gmail.com>
2022-09-15 03:28:46 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,46 +1,45 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account.
- * @month: month in number format
- * @day: day of month
- * @year: year
- *
- * You can assume that all test cases have valid months (i.e. the value,
- * of month will never be less than 1 or greater than 12) and valid,
- * days (i.e. the value of day will never be less than 1 or greater than 31).
- * You can assume that all test cases have valid month/day combinations,
- * (i.e. there will never be a June 31st or November 31st, etc.), but not,
- * all month/day/year combinations are valid (i.e. February 29, 1991 or,
- * February 29, 2427).
- *
- * Return: void.
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
-
-void print_remaining_days(int month, int day, int year)
+int largest_number(int a, int b, int c)
+  
 {
-  if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
-    {
-      if (month > 2 && day >= 60)
-	{
-	  day++;
-	}
+  
+  int largest;
+  
 
-      printf("Day of the year: %d\n", day);
-      printf("Remaining days: %d\n", 366 - day);
-    }
-  else
+  
+  if (a >= b && a > c)
+    
     {
-      if (month == 2 && day == 60)
-	{
-	  printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-      else
-	{
-	  printf("Day of the year: %d\n", day);
-	  printf("Remaining days: %d\n", 365 - day);
-	}
+      
+      largest = a;
+      
     }
+  
+  else if (b >= a && b > c)
+    
+    {
+      
+      largest = b;
+      
+    }
+  
+  else
+    
+    {
+      
+      largest = c;
+      
+    }
+  
+
+  
+  return (largest);
+  
 }
