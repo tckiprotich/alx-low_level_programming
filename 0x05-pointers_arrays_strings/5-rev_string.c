5a84ea22551301481f231e7a0f10180f5cfa6c08
collins <tckiprotich@gmail.com>
2022-09-19 03:44:47 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,77 +5,27 @@
  *
  */
 void rev_string(char *s)
-  
 {
-  
   int i;
-  
   int j;
-  
   int temp;
-  
   int length;
-  
 
-  
   while (s[i] != 0)
-    
     {
-      
       i++;
-      
     }
-  
 
-  
   length = i;
-  
   i = 0;
-  
   j = length - 1;
-  
 
-  
   while (i < j)
-    
     {
-      
       temp = s[i];
-      
       s[i] = s[j];
-      
       s[j] = temp;
-      
       i++;
-      
       j--;
-      
     }
-  
-=======
-void rev_string(char *s)
-{
-	int i;
-	int j;
-	int temp;
-	int length;
-
-	while (s[i] != 0)
-	{
-		i++;
-	}
-
-	length = i;
-	i = 0;
-	j = length - 1;
-
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
->>>>>>> 55c4bee72cfbb93be01c941bf01b2e39198b6c2b
 }
