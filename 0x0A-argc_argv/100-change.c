5421b9dd42c6c66227c01c30809bbbd74ca25185
COLLINS  TONUI <92209537+tckiprotich@users.noreply.github.com>
2022-09-30 21:32:45 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,49 +1,85 @@
-#include "main.h"
-#include <stdlib.h>
-
-/**
-*main - prints the minimum number of coins to make change
-* for an amount of money
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: returns 1 if there is an error; else returns 0
-*/
-
-int main(int argc, char *argv[])
-{
-int cents, coins = 0;
-
-if (argc != 2)
-{
-	printf("Error\n");
-	return (1);
-}
-cents = atoi(argv[1]);
-while (cents > 0)
-{
-	coins++;
-	if ((cents - 25) >= 0)
-	{
-		cents -= 25;
-		continue;
-	}
-	if ((cents - 10) >= 0)
-	{
-		cents -= 10;
-		continue;
-	}
-	if ((cents - 5) >= 0)
-	{
-		cents -= 5;
-		continue;
-	}
-	if ((cents - 2) >= 0)
-	{
-		cents -= 2;
-		continue;
-	}
-	cents--;
-}
-printf("%d\n", coins);
-return (0);
-}
+#include <stdlib.h> 
+ #include <stdio.h> 
+ #include "main.h" 
+  
+  
+  
+  
+  
+  
+ /** 
+  * coin_count - counts the min coins needed for change 
+  * 
+  * @argv: argument vector 
+  * @count: int to count coins 
+  * Return: int 
+  */ 
+ int coin_count(char *argv[], int count) 
+ { 
+ int sum = 0; 
+  
+ while (!(sum + 25 > atoi(argv[1]))) 
+ { 
+ sum += 25; 
+ count++; 
+ } 
+ while (!(sum + 10 > atoi(argv[1]))) 
+ { 
+ sum += 10; 
+ count++; 
+ } 
+ while (!(sum + 5 > atoi(argv[1]))) 
+ { 
+ sum += 5; 
+ count++; 
+ } 
+ while (!(sum + 2 > atoi(argv[1]))) 
+ { 
+ sum += 2; 
+ count++; 
+ } 
+ while (!(sum + 1 > atoi(argv[1]))) 
+ { 
+ sum += 1; 
+ count++; 
+ } 
+ return (count); 
+ } 
+  
+  
+  
+  
+  
+  
+  
+  
+ /** 
+  * main - prints the min num of coins to make change for an amount of money 
+  * 
+  * @argc: argument count 
+  * @argv: argument vector 
+  * Return: int 
+  */ 
+  
+  
+  
+  
+  
+ int main(int argc, char *argv[]) 
+ { 
+ int count = 0; 
+  
+ if (argc != 2) 
+ { 
+ printf("Error\n"); 
+ return (1); 
+ } 
+ else if (atoi(argv[1]) < 0) 
+ { 
+ printf("%d\n", 0); 
+ return (0); 
+ } 
+ count = coin_count(argv, count); 
+ printf("%d\n", count); 
+ return (0); 
+ }
