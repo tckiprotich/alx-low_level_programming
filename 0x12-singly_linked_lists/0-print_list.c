37bc5c467ccdcf623a7ea85bd3e6dc879e9aef38
tckiprotich <tckiprotich@gmail.com>
2022-10-24 13:45:07 +0300

    print-list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Prototype: size_t print_list(const list_t *h);
+Return: the number of nodes
+Format: see example
+If str is NULL, print [0] (nil)
+You are allowed to use printf
+*/
+
+int main(void)
+{
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n ")
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    free(new);
+    return (0);
+}
