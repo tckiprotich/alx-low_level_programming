97d6946b49559b56fe57eee897813e4ba4c760e7
tckiprotich <tckiprotich@gmail.com>
2022-10-24 15:10:18 +0300

    print-list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - function that prints all the elements of a list_t list
- * @h: Const pointer of structure list_t
- * Return: Number of nodes
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-
-size_t print_list(const list_t *h)
+int main(void)
 {
-	unsigned int i = 0;
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
 
-		h = h->next;
-		i++;
-	}
-
-	return (i);
+    free(new);
+    return (0);
 }
